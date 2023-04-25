05c863b3ca43484adf33a0c879f5b20d397088c2
jenweani <jenweani@gmail.com>
2023-04-25 14:36:56 -0700

    added first printf task

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # printf
+
+This repository contains solutions to the alx printf task
---
+++ _printf.c
@@ -0,0 +1,70 @@
+/*
+ * File: _printf.c
+ * Auth: John Enweani
+ */
+
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: character string
+ * @...: other args passed to the functions
+ *
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	buffer_t *output;
+	va_list args;
+	int ret;
+
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+
+	return (ret);
+}
